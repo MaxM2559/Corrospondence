3a38987a7a873175a7bbfa3ae4474428b5a16891
MaxM2559 <93101107+MaxM2559@users.noreply.github.com>
2022-07-17 19:03:00 -0400

    Update Corrospondence_Functions.py

--- Corrospondence_Functions.py
+++ Corrospondence_Functions.py
@@ -1,29 +1,7 @@
 import re
 from openpyxl import load_workbook
 
-### README:
-
-# These functions where created to mainly help clean and harmonize data
-# across different corrospondence sheets between industry codes, such as the
-# NAICS code, HS code, and more. A copy of the masterlist I created with these
-# functions will be posted on my GitHub.
-#
-# Any changes made to Excel files from Python can NOT be reversed on Excel, so
-# always create a copy of the Excel file or sheet before making any changes.
-#
-# Always save and close the Excel file before marking any alterations with Python.
-#
-# Working with Microsoft Excel through Python can be very slow at times. I
-# usually implement several print statements in my function to check if my
-# program is running correctly.
-#
-# When working with new Excel files, sometimes the file must first be opened
-# and saved before Python can alter it. Also, be sure to enable editing for
-# said Excel files.
-#
-# Python will only alter the most recently selected and saved Excel sheet unless
-# otherwise specified.
-
+### See README for more information
 
 ### put path of file here
 nameOfFile = r"file_path_here"
